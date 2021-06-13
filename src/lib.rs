70b378ce07ee0fd452b2374fd6230a9fd9b26004
Ryan <rnleach@users.noreply.github.com>
2021-06-13 17:05:26 -0600

    Update dependencies and rustfmt.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,15 @@
 #![warn(missing_docs)]
 //! Library to parse and iterate over weather soundings retrieved from [University of Wyoming][1].
 //!
-//! Retrieve a sounding from the [University of Wyoming][1] page in the "Text: List" format, and 
-//! then pass the text to the `parse_text()` function and it will return an iterator. The iterator 
+//! Retrieve a sounding from the [University of Wyoming][1] page in the "Text: List" format, and
+//! then pass the text to the `parse_text()` function and it will return an iterator. The iterator
 //! returns `(sounding_analysis::Sounding, std::collections::HashMap<&'static str, f64>)` items.
 //!
 //! The hash map contains indexes and values provided in the text that are not a part of the
 //! `sounding_analysis::Sounding` type. The keys in the hashmap should describe the values.
 //!
 //! You may build a program that automatically downloads the soundings from the website or one that
-//! loads the text from disk, either way just pass the text into the `parse_text()` function and 
+//! loads the text from disk, either way just pass the text into the `parse_text()` function and
 //! it will parse it for you.
 //!
 //! The crate was originally built to support [sonde][2], a sounding data viewer and analysis tool.
